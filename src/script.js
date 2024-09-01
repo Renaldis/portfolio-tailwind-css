99243df9084aa2aeac128233931278418df9f219
Renaldis <renaldis142@gmail.com>
2024-09-01 19:23:26 +0700

    portfolio tailwind css

---
+++ src/script.js
@@ -0,0 +1,21 @@
+// navbar fixed
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  //   header.classList.add("navbar-fixed");
+  const fixedNav = header.offsetTop;
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
+
+// HAMBURGER
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
